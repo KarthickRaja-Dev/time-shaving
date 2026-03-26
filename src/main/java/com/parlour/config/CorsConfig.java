5c28e79d875a5d0c3982459be3fc06142605899b
Karthick Raja <karthickraja@Morningstar.local>
2026-03-26 22:42:57 +0530

    cors added

--- src/main/java/com/parlour/config/CorsConfig.java
+++ src/main/java/com/parlour/config/CorsConfig.java
@@ -16,10 +16,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:3000"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setMaxAge(3600L);
